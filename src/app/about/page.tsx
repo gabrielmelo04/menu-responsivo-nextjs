80221b415ad5e605a336bdd92b1ac4461c94456b
gabrielmelo04 <gabriel_m123@hotmail.com>
2024-07-16 23:32:18 -0300

    update name about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,7 +5,7 @@ export default function About(){
     return(
         <div className="w-screen h-screen flex justify-center items-center bg-zinc-900">
             <h1 className="text-3xl font-bold text-zinc-50 tracking-widest">
-                About
+                Sobre
             </h1>
         </div>
     )
